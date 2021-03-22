a738ae01591ba2d82bb6acf8dd7cfc7f759ec507
kylewilliamson123 <79099015+kylewilliamson123@users.noreply.github.com>
2021-03-22 17:14:52 -0500

    inputs
    inputs

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -21,6 +21,24 @@
                     <label>Region</label>
                     <input type="text" name="region" class="form-control">
 
+                    <label>CWID</label>
+                    <input type="text" name="cwid" class="form-control">
+
+                    <label>Seat Location</label>
+                    <input type="text" name="seat-loc" class="form-control">
+
+                    <label>Platform</label>
+                    <input type="text" name="platform" class="form-control">
+
+                    <label>Manager</label>
+                    <input type="text" name="manager" class="form-control">
+
+                    <label>Team Name</label>
+                    <input type="text" name="team-name" class="form-control">
+
+                    <label>Hire Type</label>
+                    <input type="text" name="hire-type" class="form-control">
+
                     <label>Role</label>
                     <Select class="w-100 h-50">
                     <option value="admin">admin</option>
