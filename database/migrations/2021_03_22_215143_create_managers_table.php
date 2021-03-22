4244022df7e2a538b5be1f87a12fa1318bd341c7
Chris McLaughlin <christopher.n.mclaughlin@gmail.com>
2021-03-22 17:02:39 -0500

    created the managers migration

---
+++ database/migrations/2021_03_22_215143_create_managers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateManagersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('managers', function (Blueprint $table)
+        {
+            //standard ID of the managers in the db
+            $table->id();
+
+            //the name of the managers in the database
+            $table->char('Name', 255);
+
+            
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('managers');
+    }
+}
