ebcf9ab4b98d40ff3cddc6e79ceade40b1b83e66
kylewilliamson123 <79099015+kylewilliamson123@users.noreply.github.com>
2021-04-05 20:30:42 -0500

    side view
    in add onboard.

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -6,10 +6,10 @@
         
         
         <!-- Left Column -->    
-        <div class="col-md-6 ">
+        <div class="col-md-4 ">
             <div class="content bg-blue">
             
-                <h2>Add New Onboard</h2>
+                <h2>Basic Information</h2>
                 <form action="{{ route('formSubmit') }}" method="post">
                     @csrf
                     
@@ -28,20 +28,61 @@
                     <label for="region">Region</label>
                     <input id="region" type="text" name="region" class="form-control">
 
+                    
+
+                    
+                </form>
+
+
+            </div>
+        </div>
+        
+
+        <div class="col-md-4">
+            <div class="content bg-blue">
+                <h3>Onboard Information</h3>
+                    <form action="{{ route('formSubmit') }}" method="post">
+                    @csrf
+
+                    
                     <label for="cwid">CWID</label>
                     <input id="cwid" type="text" name="cwid" class="form-control">
 
+                    <label for="hire-type">Hire Type</label>
+                    <input id="hire-type" type="text" name="hire-type" class="form-control">
+
+                    <label for="hire-status">Hire Status</label>
+                    <input id="cwid" type="text" name="cwid" class="form-control">
+
+                    <label for="platform">Platform</label>
+                    <select id="platform" class="form-control">
+                        <option value="mac">Mac</option>
+                        <option value="windows">Windows</option>
+                        <option value="lenovo">Lenovo</option>
+                    </select>
+
+                    <label for="manager-comments">Manager Comments</label>
+                    <textarea class="form-control" rows="5">
+                    </textarea>
+                    </form>
+        </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="content bg-blue">
+                <h3>Job Information</h3>
+                    <form action="{{ route('formSubmit') }}" method="post">
+                    @csrf
+
+                    <label for="team-name">Team Name</label>
+                    <input id="team-name" type="text" name="team-name" class="form-control">
+      
                     <label for="seat-loc">Seat Number</label>
                     <input id="seat-loc" type="number" name="seat-loc" class="form-control">
 
                     <label for="manager">Manager</label>
                     <input id="manager" type="text" name="manager" class="form-control">
 
-                    <label for="team-name">Team Name</label>
-                    <input id="team-name" type="text" name="team-name" class="form-control">
-
-                    <label for="hire-type">Hire Type</label>
-                    <input id="hire-type" type="text" name="hire-type" class="form-control">
 
                     <label for="vendor">Vendor</label>
                     <select id="vendor" class="form-control">
@@ -50,13 +91,6 @@
                         <option value="nbc">NBC</option>
                         <option value="na">N/A</option>
                     </select>
-    
-                    <label for="platform">Platform</label>
-                    <select id="platform" class="form-control">
-                        <option value="mac">Mac</option>
-                        <option value="windows">Windows</option>
-                        <option value="lenovo">Lenovo</option>
-                    </select>
 
                     <label for="">Role</label>
                     <select class="form-control">
@@ -64,17 +98,13 @@
                         <option value="programmer">Programmer</option>
                     </select>
 
-                    <label for="">Manager Comments</label>
-                    <textarea class="form-control" rows="5">
-                    </textarea>
-
                     <input type="submit" class="btn btn-secondary mt-4 form-control" value="Submit">
-                </form>
-
-
-            </div>
-        </div>
-
+                    </form>
+                    </div>
+                    </div>
     </div>
 </div>
 @endsection
+
+
+
