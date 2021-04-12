75049fb93a88cbe76500cd1818d82145200617bd
Chris McLaughlin <christopher.n.mclaughlin@gmail.com>
2021-04-12 17:56:51 -0500

    Finished search function!

--- app/Http/Controllers/FinishedController.php
+++ app/Http/Controllers/FinishedController.php
@@ -27,23 +27,17 @@ class FinishedController extends Controller
         //the loop to store the results in $results
         $results = new_hire::where('FirstName','LIKE','%'.$search .'%')
         ->orWhere('LastName','LIKE','%'.$search.'%')
-        ->orWhere('CWID','=',$search);
-
-        //to test, add this to the where search above
-        //->first();
-        //die($results);
+        ->orWhere('CWID','LIKE','%'.$search.'%')->get();
 
         //if there is more than 1 result, display
-        if (count ($results) > 0)
+        if(is_array($results))
         {
-        
             return view('finished')->withDetails($results)->withQuery($search);
         }
         //if there is no results, return this message 
         else
         {
             return view('finished')->withMessage('No Results found.');
-            
         }
     }// end search
 }
